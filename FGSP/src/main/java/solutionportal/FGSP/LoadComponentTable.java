f9714712e65a10e1219f9f51bf7c4e98e812bce2
Akim0992 <akim.neghouche@gmail.com>
2017-10-19 11:36:03 +0200

    test 1
    test 1

--- FGSP/src/main/java/solutionportal/FGSP/LoadComponentTable.java
+++ FGSP/src/main/java/solutionportal/FGSP/LoadComponentTable.java
@@ -1,5 +1,19 @@
 package solutionportal.FGSP;
 
+import java.sql.Statement;
+
+import org.apache.poi.ss.usermodel.Row;
+
 public class LoadComponentTable {
 
+	public Statement statement;
+	
+	LoadComponentTable(Statement s) {
+		this.statement = s;
+	}
+	
+	public void loadDataInComponentTable(String id, Row row) {
+	
+	}
+	
 }
